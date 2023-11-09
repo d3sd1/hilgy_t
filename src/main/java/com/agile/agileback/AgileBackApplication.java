91e472de1f7659a3746a2ec598135ac6253abb98
andrei <Your Email>
2023-11-09 18:33:44 +0100

    INITIAL COMMIT

---
+++ src/main/java/com/agile/agileback/AgileBackApplication.java
@@ -0,0 +1,13 @@
+package com.agile.agileback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agile/agileback/AgileBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agile.agileback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
